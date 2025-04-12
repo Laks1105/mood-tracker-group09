7ce3028961f4db3fc98c2dffdc3a9c533c2a16d0
Laks1105 <pblaks1105@gmail.com>
2025-04-13 01:18:42 +0800

    Add files via upload

---
+++ testing code.py
@@ -0,0 +1,11 @@
+import os
+while True:
+    opt=int(input("select between 1 and 5:"))
+    if opt==1:
+        print("hello world 1")
+        break 
+    elif opt ==5:
+        print ("hello world 5")
+        break
+    else: 
+        print("invalid answer")
